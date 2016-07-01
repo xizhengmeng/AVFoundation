254c0bb28de4f3d9ab2c82ba40e67e6d7f7a461c
Hanson <hanshenghui647@gmail.com>
2016-07-01 12:59:23 +0800

    切换摄像头功能

--- AVFoundation/AVFoundation/Section/Camera/ViewController/CameraViewController.m
+++ AVFoundation/AVFoundation/Section/Camera/ViewController/CameraViewController.m
@@ -7,7 +7,480 @@
 //
 
 #import "CameraViewController.h"
+typedef void(^PropertyChangeBlock)(AVCaptureDevice *captureDevice);
+@interface CameraViewController()
+@property (strong,nonatomic) AVCaptureSession *captureSession;//负责输入和输出设备之间的数据传递
+@property (strong,nonatomic) AVCaptureDeviceInput *captureDeviceInput;//负责从AVCaptureDevice获得输入数据
+@property (strong,nonatomic) AVCaptureStillImageOutput *captureStillImageOutput;//照片输出流
+@property (strong,nonatomic) AVCaptureVideoPreviewLayer *captureVideoPreviewLayer;//相机拍摄预览图层
+@property (strong, nonatomic) UIView *viewContainer;
+@property (strong, nonatomic) UIButton *takeButton;//拍照按钮
+@property (strong, nonatomic) UIButton *flashAutoButton;//自动闪光灯按钮
+@property (strong, nonatomic) UIButton *flashOnButton;//打开闪光灯按钮
+@property (strong, nonatomic) UIButton *flashOffButton;//关闭闪光灯按钮
+@property (strong, nonatomic) UIImageView *focusCursor; //聚焦光标
+@property (nonatomic, strong) UIButton *changeBtn;
+
+@end
 
 @implementation CameraViewController
+
+#pragma mark - 控制器视图方法
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.viewContainer];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    //初始化会话
+    _captureSession=[[AVCaptureSession alloc]init];
+    if ([_captureSession canSetSessionPreset:AVCaptureSessionPreset1280x720]) {//设置分辨率
+        _captureSession.sessionPreset=AVCaptureSessionPreset1280x720;
+    }
+    //获得输入设备
+    AVCaptureDevice *captureDevice=[self getCameraDeviceWithPosition:AVCaptureDevicePositionBack];//取得后置摄像头
+    if (!captureDevice) {
+        NSLog(@"取得后置摄像头时出现问题.");
+        return;
+    }
+    
+    NSError *error=nil;
+    //根据输入设备初始化设备输入对象，用于获得输入数据
+    _captureDeviceInput=[[AVCaptureDeviceInput alloc]initWithDevice:captureDevice error:&error];
+    if (error) {
+        NSLog(@"取得设备输入对象时出错，错误原因：%@",error.localizedDescription);
+        return;
+    }
+    //初始化设备输出对象，用于获得输出数据
+    _captureStillImageOutput=[[AVCaptureStillImageOutput alloc]init];
+    NSDictionary *outputSettings = @{AVVideoCodecKey:AVVideoCodecJPEG};
+    [_captureStillImageOutput setOutputSettings:outputSettings];//输出设置
+    
+    //将设备输入添加到会话中
+    if ([_captureSession canAddInput:_captureDeviceInput]) {
+        [_captureSession addInput:_captureDeviceInput];
+    }
+    
+    //将设备输出添加到会话中
+    if ([_captureSession canAddOutput:_captureStillImageOutput]) {
+        [_captureSession addOutput:_captureStillImageOutput];
+    }
+    
+    //创建视频预览层，用于实时展示摄像头状态
+    _captureVideoPreviewLayer=[[AVCaptureVideoPreviewLayer alloc]initWithSession:self.captureSession];
+    
+    CALayer *layer=self.viewContainer.layer;
+    layer.masksToBounds=YES;
+    
+    _captureVideoPreviewLayer.frame=layer.bounds;
+    _captureVideoPreviewLayer.videoGravity=AVLayerVideoGravityResizeAspectFill;//填充模式
+    //将视频预览层添加到界面中
+    //[layer addSublayer:_captureVideoPreviewLayer];
+    [layer addSublayer:self.focusCursor.layer];
+    [layer insertSublayer:_captureVideoPreviewLayer below:self.focusCursor.layer];
+    
+    [self addNotificationToCaptureDevice:captureDevice];
+    [self addGenstureRecognizer];
+    [self setFlashModeButtonStatus];
+    
+    [_viewContainer addSubview:self.takeButton];
+    self.takeButton.bottom = kScreenH - 100;
+    self.takeButton.centerX = kScreenW * 0.5;
+    
+    [_viewContainer addSubview:self.changeBtn];
+    self.changeBtn.bottom = self.takeButton.bottom;
+    self.changeBtn.x = self.takeButton.right + 20;
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    [self.captureSession startRunning];
+}
+
+-(void)viewDidDisappear:(BOOL)animated{
+    [super viewDidDisappear:animated];
+    [self.captureSession stopRunning];
+}
+
+-(void)dealloc{
+    [self removeNotification];
+}
+#pragma mark - UI方法
+#pragma mark 拍照
+- (void)takeButtonClick:(UIButton *)sender {
+    //根据设备输出获得连接
+    AVCaptureConnection *captureConnection=[self.captureStillImageOutput connectionWithMediaType:AVMediaTypeVideo];
+    //根据连接取得设备输出的数据
+    [self.captureStillImageOutput captureStillImageAsynchronouslyFromConnection:captureConnection completionHandler:^(CMSampleBufferRef imageDataSampleBuffer, NSError *error) {
+        if (imageDataSampleBuffer) {
+            NSData *imageData=[AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageDataSampleBuffer];
+            UIImage *image=[UIImage imageWithData:imageData];
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+            //            ALAssetsLibrary *assetsLibrary=[[ALAssetsLibrary alloc]init];
+            //            [assetsLibrary writeImageToSavedPhotosAlbum:[image CGImage] orientation:(ALAssetOrientation)[image imageOrientation] completionBlock:nil];
+        }
+        
+    }];
+}
+#pragma mark 切换前后摄像头
+- (void)toggleButtonClick:(UIButton *)sender {
+    
+    AVCaptureDevice *currentDevice=[self.captureDeviceInput device];
+    AVCaptureDevicePosition currentPosition=[currentDevice position];
+    [self removeNotificationFromCaptureDevice:currentDevice];
+    AVCaptureDevice *toChangeDevice;
+    AVCaptureDevicePosition toChangePosition=AVCaptureDevicePositionFront;
+    if (currentPosition==AVCaptureDevicePositionUnspecified||currentPosition==AVCaptureDevicePositionFront) {
+        toChangePosition=AVCaptureDevicePositionBack;
+    }
+    toChangeDevice=[self getCameraDeviceWithPosition:toChangePosition];
+    [self addNotificationToCaptureDevice:toChangeDevice];
+    //获得要调整的设备输入对象
+    AVCaptureDeviceInput *toChangeDeviceInput=[[AVCaptureDeviceInput alloc]initWithDevice:toChangeDevice error:nil];
+    
+    //改变会话的配置前一定要先开启配置，配置完成后提交配置改变
+    [self.captureSession beginConfiguration];
+    //移除原有输入对象
+    [self.captureSession removeInput:self.captureDeviceInput];
+    //添加新的输入对象
+    if ([self.captureSession canAddInput:toChangeDeviceInput]) {
+        [self.captureSession addInput:toChangeDeviceInput];
+        self.captureDeviceInput=toChangeDeviceInput;
+    }
+    
+    //提交会话配置
+    [self.captureSession commitConfiguration];
+    //设置闪光灯按钮状态
+    [self setFlashModeButtonStatus];
+}
+
+#pragma mark 自动闪光灯开启
+- (IBAction)flashAutoClick:(UIButton *)sender {
+    [self setFlashMode:AVCaptureFlashModeAuto];
+    [self setFlashModeButtonStatus];
+}
+#pragma mark 打开闪光灯
+- (IBAction)flashOnClick:(UIButton *)sender {
+    [self setFlashMode:AVCaptureFlashModeOn];
+    [self setFlashModeButtonStatus];
+}
+#pragma mark 关闭闪光灯
+- (IBAction)flashOffClick:(UIButton *)sender {
+    [self setFlashMode:AVCaptureFlashModeOff];
+    [self setFlashModeButtonStatus];
+}
+
+#pragma mark - 通知
+/**
+ *  给输入设备添加通知
+ */
+-(void)addNotificationToCaptureDevice:(AVCaptureDevice *)captureDevice{
+    //注意添加区域改变捕获通知必须首先设置设备允许捕获
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        captureDevice.subjectAreaChangeMonitoringEnabled=YES;
+    }];
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    //捕获区域发生改变
+    [notificationCenter addObserver:self selector:@selector(areaChange:) name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+-(void)removeNotificationFromCaptureDevice:(AVCaptureDevice *)captureDevice{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self name:AVCaptureDeviceSubjectAreaDidChangeNotification object:captureDevice];
+}
+/**
+ *  移除所有通知
+ */
+-(void)removeNotification{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    [notificationCenter removeObserver:self];
+}
+
+-(void)addNotificationToCaptureSession:(AVCaptureSession *)captureSession{
+    NSNotificationCenter *notificationCenter= [NSNotificationCenter defaultCenter];
+    //会话出错
+    [notificationCenter addObserver:self selector:@selector(sessionRuntimeError:) name:AVCaptureSessionRuntimeErrorNotification object:captureSession];
+}
+
+/**
+ *  设备连接成功
+ *
+ *  @param notification 通知对象
+ */
+-(void)deviceConnected:(NSNotification *)notification{
+    NSLog(@"设备已连接...");
+}
+/**
+ *  设备连接断开
+ *
+ *  @param notification 通知对象
+ */
+-(void)deviceDisconnected:(NSNotification *)notification{
+    NSLog(@"设备已断开.");
+}
+/**
+ *  捕获区域改变
+ *
+ *  @param notification 通知对象
+ */
+-(void)areaChange:(NSNotification *)notification{
+    NSLog(@"捕获区域改变...");
+}
+
+/**
+ *  会话出错
+ *
+ *  @param notification 通知对象
+ */
+-(void)sessionRuntimeError:(NSNotification *)notification{
+    NSLog(@"会话发生错误.");
+}
+
+#pragma mark - 私有方法
+
+/**
+ *  取得指定位置的摄像头
+ *
+ *  @param position 摄像头位置
+ *
+ *  @return 摄像头设备
+ */
+-(AVCaptureDevice *)getCameraDeviceWithPosition:(AVCaptureDevicePosition )position{
+    NSArray *cameras= [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
+    for (AVCaptureDevice *camera in cameras) {
+        if ([camera position]==position) {
+            return camera;
+        }
+    }
+    return nil;
+}
+
+/**
+ *  改变设备属性的统一操作方法
+ *
+ *  @param propertyChange 属性改变操作
+ */
+-(void)changeDeviceProperty:(PropertyChangeBlock)propertyChange{
+    AVCaptureDevice *captureDevice= [self.captureDeviceInput device];
+    NSError *error;
+    //注意改变设备属性前一定要首先调用lockForConfiguration:调用完之后使用unlockForConfiguration方法解锁
+    if ([captureDevice lockForConfiguration:&error]) {
+        propertyChange(captureDevice);
+        [captureDevice unlockForConfiguration];
+    }else{
+        NSLog(@"设置设备属性过程发生错误，错误信息：%@",error.localizedDescription);
+    }
+}
+
+/**
+ *  设置闪光灯模式
+ *
+ *  @param flashMode 闪光灯模式
+ */
+-(void)setFlashMode:(AVCaptureFlashMode )flashMode{
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        if ([captureDevice isFlashModeSupported:flashMode]) {
+            [captureDevice setFlashMode:flashMode];
+        }
+    }];
+}
+/**
+ *  设置聚焦模式
+ *
+ *  @param focusMode 聚焦模式
+ */
+-(void)setFocusMode:(AVCaptureFocusMode )focusMode{
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        if ([captureDevice isFocusModeSupported:focusMode]) {
+            [captureDevice setFocusMode:focusMode];
+        }
+    }];
+}
+/**
+ *  设置曝光模式
+ *
+ *  @param exposureMode 曝光模式
+ */
+-(void)setExposureMode:(AVCaptureExposureMode)exposureMode{
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        if ([captureDevice isExposureModeSupported:exposureMode]) {
+            [captureDevice setExposureMode:exposureMode];
+        }
+    }];
+}
+/**
+ *  设置聚焦点
+ *
+ *  @param point 聚焦点
+ */
+-(void)focusWithMode:(AVCaptureFocusMode)focusMode exposureMode:(AVCaptureExposureMode)exposureMode atPoint:(CGPoint)point{
+    [self changeDeviceProperty:^(AVCaptureDevice *captureDevice) {
+        if ([captureDevice isFocusModeSupported:focusMode]) {
+            [captureDevice setFocusMode:AVCaptureFocusModeAutoFocus];
+        }
+        if ([captureDevice isFocusPointOfInterestSupported]) {
+            [captureDevice setFocusPointOfInterest:point];
+        }
+        if ([captureDevice isExposureModeSupported:exposureMode]) {
+            [captureDevice setExposureMode:AVCaptureExposureModeAutoExpose];
+        }
+        if ([captureDevice isExposurePointOfInterestSupported]) {
+            [captureDevice setExposurePointOfInterest:point];
+        }
+    }];
+}
+
+/**
+ *  添加点按手势，点按时聚焦
+ */
+-(void)addGenstureRecognizer{
+    UITapGestureRecognizer *tapGesture=[[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tapScreen:)];
+    [self.viewContainer addGestureRecognizer:tapGesture];
+}
+-(void)tapScreen:(UITapGestureRecognizer *)tapGesture{
+    CGPoint point= [tapGesture locationInView:self.viewContainer];
+    //将UI坐标转化为摄像头坐标
+    CGPoint cameraPoint= [self.captureVideoPreviewLayer captureDevicePointOfInterestForPoint:point];
+    [self setFocusCursorWithPoint:point];
+    [self focusWithMode:AVCaptureFocusModeAutoFocus exposureMode:AVCaptureExposureModeAutoExpose atPoint:cameraPoint];
+}
+
+/**
+ *  设置闪光灯按钮状态
+ */
+-(void)setFlashModeButtonStatus{
+    AVCaptureDevice *captureDevice=[self.captureDeviceInput device];
+    AVCaptureFlashMode flashMode=captureDevice.flashMode;
+    if([captureDevice isFlashAvailable]){
+        self.flashAutoButton.hidden=NO;
+        self.flashOnButton.hidden=NO;
+        self.flashOffButton.hidden=NO;
+        self.flashAutoButton.enabled=YES;
+        self.flashOnButton.enabled=YES;
+        self.flashOffButton.enabled=YES;
+        switch (flashMode) {
+            case AVCaptureFlashModeAuto:
+                self.flashAutoButton.enabled=NO;
+                break;
+            case AVCaptureFlashModeOn:
+                self.flashOnButton.enabled=NO;
+                break;
+            case AVCaptureFlashModeOff:
+                self.flashOffButton.enabled=NO;
+                break;
+            default:
+                break;
+        }
+    }else{
+        self.flashAutoButton.hidden=YES;
+        self.flashOnButton.hidden=YES;
+        self.flashOffButton.hidden=YES;
+    }
+}
+
+/**
+ *  设置聚焦光标位置
+ *
+ *  @param point 光标位置
+ */
+-(void)setFocusCursorWithPoint:(CGPoint)point{
+    self.focusCursor.center=point;
+    self.focusCursor.transform=CGAffineTransformMakeScale(1.5, 1.5);
+    self.focusCursor.alpha=1.0;
+    [UIView animateWithDuration:1.0 animations:^{
+        self.focusCursor.transform=CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+        self.focusCursor.alpha=0;
+        
+    }];
+}
+
+- (UIView *)viewContainer {
+    if (!_viewContainer) {
+        _viewContainer = [[UIView alloc] initWithFrame:self.view.bounds];
+        
+    }
+    return _viewContainer;
+}
+
+- (UIButton *)takeButton {
+    if (!_takeButton) {
+        _takeButton = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_takeButton setTitle:@"拍摄" forState:UIControlStateNormal];
+        [_takeButton setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        _takeButton.width += 20;
+        _takeButton.height += 10;
+        _takeButton.layer.borderColor = [UIColor orangeColor].CGColor;
+        _takeButton.layer.borderWidth = 1;
+        [_takeButton sizeToFit];
+        [_takeButton addTarget:self action:@selector(takeButtonClick:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _takeButton;
+}
+
+- (UIButton *)changeBtn {
+    if (!_changeBtn) {
+        _changeBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_changeBtn setTitle:@"切换摄像头" forState:UIControlStateNormal];
+        [_changeBtn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+        _changeBtn.width += 20;
+        _changeBtn.height += 10;
+        _changeBtn.layer.borderColor = [UIColor orangeColor].CGColor;
+        _changeBtn.layer.borderWidth = 1;
+        [_changeBtn sizeToFit];
+        [_changeBtn addTarget:self action:@selector(perspect3D) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return _changeBtn;
+}
+
+- (UIImageView *)focusCursor {
+    if (!_focusCursor) {
+        _focusCursor = [[UIImageView alloc] init];
+        _focusCursor.backgroundColor = [[UIColor blueColor] colorWithAlphaComponent:0.2];
+//        _focusCursor.size  =CGSizeMake(100, 100);
+    }
+    return _focusCursor;
+}
+
+//3D旋转效果
+CATransform3D CATransform3DMakePerspective(CGPoint center, float disZ)
+{
+    CATransform3D transToCenter = CATransform3DMakeTranslation(-center.x, -center.y, 0);
+    CATransform3D transBack = CATransform3DMakeTranslation(center.x, center.y, 0);
+    CATransform3D scale = CATransform3DIdentity;
+    scale.m34 = -1.0f/disZ;
+    return CATransform3DConcat(CATransform3DConcat(transToCenter, scale), transBack);
+}
+
+CATransform3D CATransform3DPerspect(CATransform3D t, CGPoint center, float disZ)
+{
+    return CATransform3DConcat(t, CATransform3DMakePerspective(center, disZ));
+}
+//3D旋转效果
+- (void)perspect3D {
+    WEAKSELF;
+    self.viewContainer.layer.doubleSided = YES;
+    [UIView animateWithDuration:0.8f animations:^{
+        
+        CATransform3D transloate0 = CATransform3DMakeTranslation(0, 0, 0);
+        CATransform3D rotate0 = CATransform3DMakeRotation(M_PI, 0, 1, 0); //旋转180度将正面的未签到图转到背面
+        CATransform3D mat0 = CATransform3DConcat(rotate0, transloate0);
+        self.viewContainer.layer.transform = CATransform3DPerspect(mat0, CGPointMake(0, 0), 500);
+        
+        CATransform3D transloate1 = CATransform3DMakeTranslation(0, 0, 0);
+        CATransform3D rotate1 = CATransform3DMakeRotation(M_PI*2, 0, 1, 0); //旋转到360度，也就是背面的已签到图转到正面
+        CATransform3D mat1 = CATransform3DConcat(rotate1, transloate1);
+        self.viewContainer.layer.transform = CATransform3DPerspect(mat1, CGPointMake(0, 0), 500);
+    } completion:^(BOOL finished) {
+        //        [self.blueView removeFromSuperview]; // 临时复制出来的view销毁
+        
+    }];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 0.05 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
+        [weakSelf performSelector:@selector(toggleButtonClick:) withObject:nil];
+    });
+}
 
 @end
